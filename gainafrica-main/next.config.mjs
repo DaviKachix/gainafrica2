f2399147e0150ed0dfcc68092e64de925ecaf8d5
countryBI <nextcountry78@>
2026-02-05 18:59:22 +0300

    git add next.config.mjs

--- gainafrica-main/next.config.mjs
+++ gainafrica-main/next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    appDir: true, // ← enable App Router
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
